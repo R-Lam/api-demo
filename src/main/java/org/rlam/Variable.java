1d8e60ed5de3a2b71f29bc24151c75d41987721d
Ricardo Lam <ricardolam3@gmail.com>
2022-04-18 21:15:30 -0600

    Add qute templates

---
+++ src/main/java/org/rlam/Variable.java
@@ -0,0 +1,15 @@
+package org.rlam;
+
+public class Variable{
+
+    public String name;
+    public String value;
+
+    public Variable() {
+    }
+
+    public Variable(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
